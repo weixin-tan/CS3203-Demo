a107660702c00964421698a02fed1c7b5abcbdfd
weixin-tan <weixin_tan99@hotmail.com>
2022-02-03 23:19:54 +0800

    Update PkbGetter to use updated enum naming

--- Team35/Code35/src/spa/src/PKB/PkbGetter.h
+++ Team35/Code35/src/spa/src/PKB/PkbGetter.h
@@ -4,14 +4,13 @@
 #include <vector>
 #include "Entity.h"
 
-// TODO: update once enum naming is fixed
 class PkbGetter {
-    virtual bool isRelationship(relationshipType r, Entity leftSide, Entity rightSide) = 0;
-    virtual std::vector<Entity> getEntity(entityType typeToGet) = 0;
-    // TODO: does not explicitly returns statements
-    virtual std::vector<Entity> getRelationshipStatements(relationshipType r) = 0;
-    virtual std::vector<Entity> getLeftSide(relationshipType r, Entity rightSide, entityType typeToGet) = 0;
-    virtual std::vector<Entity> getRightSide(relationshipType r, Entity leftSide, entityType typeToGet) = 0;
+    virtual bool isRelationship(RelationshipType r, Entity leftSide, Entity rightSide) = 0;
+    virtual std::vector<Entity> getEntity(EntityType typeToGet) = 0;
+    // TODO: does not explicitly returns statements, maybe use template?
+    virtual std::vector<Entity> getRelationshipStatements(RelationshipType r) = 0;
+    virtual std::vector<Entity> getLeftSide(RelationshipType r, Entity rightSide, EntityType typeToGet) = 0;
+    virtual std::vector<Entity> getRightSide(RelationshipType r, Entity leftSide, EntityType typeToGet) = 0;
 };
 
 #endif //SPA_PKBGETTER_H
