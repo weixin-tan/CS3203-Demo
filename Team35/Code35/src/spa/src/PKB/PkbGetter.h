65cb3fafeabbdea26bfd501ea49ad3258c8374b0
weixin-tan <weixin_tan99@hotmail.com>
2022-02-04 16:43:25 +0800

    Minor update

--- Team35/Code35/src/spa/src/PKB/PkbGetter.h
+++ Team35/Code35/src/spa/src/PKB/PkbGetter.h
@@ -13,7 +13,7 @@ public:
   explicit PkbGetter(DB* db);
   // TODO: change to pass by reference or pointer
   bool isRelationship(const RelationshipType& r, const Entity& leftSide, const Entity& rightSide) const;
-  std::vector<Entity> getEntity(const EntityType& typeToGet) const;
+//  std::vector<Entity> getEntity(const EntityType& typeToGet) const;
   // TODO: does not explicitly returns statements, maybe use template?
 //  std::vector<Entity> getRelationshipStatements(RelationshipType r);
 //  std::vector<Entity> getLeftSide(RelationshipType r, Entity rightSide, EntityType typeToGet);
