986e586e5907f8325ab6431b9320ce0511a9ae9c
Vivegan <77456578+vivegank@users.noreply.github.com>
2022-02-07 19:28:24 +0800

    Integrate QPS into the TestWrapper

--- Team35/Code35/src/autotester/src/TestWrapper.cpp
+++ Team35/Code35/src/autotester/src/TestWrapper.cpp
@@ -14,6 +14,7 @@ volatile bool AbstractWrapper::GlobalStop = false;
 
 // a default constructor
 TestWrapper::TestWrapper(): pkb(), convertor(pkb.getSetter()) {
+  TestWrapper::qpsMainLogic = QPSMainLogic::getInstance(pkb.getGetter());
 }
 
 // method for parsing the SIMPLE source
@@ -24,9 +25,6 @@ void TestWrapper::parse(std::string filename) {
 
 // method to evaluating a query
 void TestWrapper::evaluate(std::string query, std::list<std::string>& results){
-// call your evaluator to evaluate the query here
-  // ...code to evaluate query...
-
-  // store the answers to the query in the results list (it is initially empty)
-  // each result must be a string.
+  std::string resultStr = qpsMainLogic->parse(query);
+  results.push_back(resultStr);
 }
