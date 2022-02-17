e4927038f222fc3082489dec6f6c208242042f20
Vivegan <77456578+vivegank@users.noreply.github.com>
2022-02-18 04:30:47 +0800

    Create EntityToElementConverter class

---
+++ Team35/Code35/src/spa/src/EntityToElementConverter.h
@@ -0,0 +1,21 @@
+#ifndef SPA_ENTITYTOELEMENTCONVERTER_H
+#define SPA_ENTITYTOELEMENTCONVERTER_H
+
+
+#include "ProgramElement.h"
+#include "Entity.h"
+
+class EntityToElementConverter {
+ private:
+  EntityToElementConverter();
+
+ public:
+  static ElementType extractElementType(const Entity& e);
+  static ProgramElement fixedEntityConverter(const Entity& e);
+
+
+
+};
+
+
+#endif //SPA_ENTITYTOELEMENTCONVERTER_H
