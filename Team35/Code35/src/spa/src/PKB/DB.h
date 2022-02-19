6569607b3d87eed993391b8462e57d206db63f18
weixin-tan <weixin_tan99@hotmail.com>
2022-02-19 18:43:38 +0800

    Implement PKB fixes based on unit testing

--- Team35/Code35/src/spa/src/PKB/DB.h
+++ Team35/Code35/src/spa/src/PKB/DB.h
@@ -10,32 +10,34 @@
 
 // just a dataclass to hold all the tables
 struct DB {
- public:
-  // relationship tables
-  std::map<int, ParsedStatement> stmtTable;
-  std::map<int, std::set<std::string>> modifyStmtToVarTable;
-  std::map<std::string, std::set<std::string>> modifyProcToVarTable;
-  std::map<std::string, std::set<int>> varToModifyStmtTable;
-  std::map<std::string, std::set<std::string>> varToModifyProcTable;
-  std::map<int, std::set<std::string>> usesStmtToVarTable;
-  std::map<std::string, std::set<std::string>> usesProcToVarTable;
-  std::map<std::string, std::set<int>> varToUsesStmtTable;
-  std::map<std::string, std::set<std::string>> varToUsesProcTable;
-  std::map<int, ElementType> stmtTypeTable;
-  std::map<int, std::set<int>> parentToChildTable;
-  std::map<int, int> childToParentTable;
-  std::map<int, std::string> stmtToProcedureCalled;
-  std::map<int, int> stmtPreceding;
-  std::map<int, int> stmtFollowing;
-  std::map<std::string, std::set<int>> constantToStmtTable;
+    // TODO: do setter/getter for the table as well
+public:
+    // relationship tables
+    std::map<int, ParsedStatement> stmtTable;
+    std::map<int, std::set<std::string>> modifyStmtToVarTable;
+    std::map<std::string, std::set<std::string>> modifyProcToVarTable;
+    std::map<std::string, std::set<int>> varToModifyStmtTable;
+    std::map<std::string, std::set<std::string>> varToModifyProcTable;
+    std::map<int, std::set<std::string>> usesStmtToVarTable;
+    std::map<std::string, std::set<std::string>> usesProcToVarTable;
+    std::map<std::string, std::set<int>> varToUsesStmtTable;
+    std::map<std::string, std::set<std::string>> varToUsesProcTable;
+    std::map<int, ElementType> stmtTypeTable;
+    std::map<int, std::set<int>> parentToChildTable;
+    std::map<int, int> childToParentTable;
+    std::map<int, std::string> stmtToProcedureCalled;
+    std::map<int, int> stmtPreceding;
+    std::map<int, int> stmtFollowing;
+    std::map<std::string, std::set<int>> constantToStmtTable;
+    std::map<int, std::set<std::string>> usesStmtToConstantTable;
 
-  // entity tables
-  std::set<std::string> variables;
-  std::set<std::string> procedures;
-  std::set<std::string> constants;
+    // entity tables
+    std::set<std::string> variables;
+    std::set<std::string> procedures;
+    std::set<std::string> constants;
 
- public:
-  DB();
+public:
+    DB();
 };
 
 
