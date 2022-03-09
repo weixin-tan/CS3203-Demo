50a4036bbae76ff477e19f40c1a870ecb15f2e4b
weixin-tan <weixin_tan99@hotmail.com>
2022-03-09 15:54:38 +0800

    Revert "Add helpful error message print and exits gracefully"
    This reverts commit bc2380d5eb4b94ed7cd26bd797225acc655f8194.

--- Team35/Code35/src/spa/src/PKB/PkbValidator.cpp
+++ Team35/Code35/src/spa/src/PKB/PkbValidator.cpp
@@ -5,7 +5,7 @@ PkbValidator::PkbValidator(DB *db) : db(db) {}
 void PkbValidator::validateNoCyclicCall() {
     for (const auto&[proc, calledTProc] : db->callsTTable) {
         if (calledTProc.count(proc) != 0)
-            throw std::invalid_argument("Cyclic call detected! Offending procedure: " + proc);
+            throw std::invalid_argument("Cyclic call detected!\n" + proc);
     }
 }
 
@@ -17,7 +17,7 @@ void PkbValidator::validateCallsExists() {
 
     for (const auto& calledProc : calledProcs)
         if (db->procedures.count(calledProc) == 0)
-            throw std::invalid_argument("Undefined " + calledProc + " procedure called!");
+            throw std::invalid_argument("Undefined procedure called!");
 }
 
 void PkbValidator::validateNoDuplicateProcedure(const std::vector<std::vector<ParsedStatement>>& procedures) {
@@ -25,7 +25,7 @@ void PkbValidator::validateNoDuplicateProcedure(const std::vector<std::vector<Pa
     for (const auto& procedure : procedures) {
         std::string proc = procedure.begin()->procedure_name;
         if (definedProcs.count(proc) != 0)
-            throw std::invalid_argument("Procedure " + proc + " redefined!\n");
+            throw std::invalid_argument("Procedure redefined!\n" + proc);
         definedProcs.insert(proc);
     }
 }
--- Team35/Code35/src/spa/src/SP.cpp
+++ Team35/Code35/src/spa/src/SP.cpp
@@ -9,33 +9,25 @@ SP::SP(PkbSetter* pkb_setter) : tokeniser(), concrete(), convertor(pkb_setter) {
 }
 
 void SP::Parse(std::string filename) {
-    try {
-        std::ifstream t(filename);
-        std::stringstream buffer;
-        buffer << t.rdbuf();
-        ProcedureLst procedureLst;
+	std::ifstream t(filename);
+	std::stringstream buffer;
+	buffer << t.rdbuf();
+	ProcedureLst procedureLst;
 
-        // Creating the queue of tokens
-        tokenQueue = tokeniser.putInQueue(buffer.str());
+	// Creating the queue of tokens
+	tokenQueue = tokeniser.putInQueue(buffer.str());
+	
+	// Create Program
+	try {
+		program = concrete.parseProgram(tokenQueue);
+	}
+	catch (const std::invalid_argument& e) {
+		std::terminate();
+	}
 
-        // Create Program
-        try {
-            program = concrete.parseProgram(tokenQueue);
-        }
-        catch (const std::invalid_argument& e) {
-            std::terminate();
-        }
+	procedureLst = program.getProcedureLst();
 
-        procedureLst = program.getProcedureLst();
-
-        // Convert the procedure
-        convertor.ProcedureReader(procedureLst);
-    } catch (const std::exception& e) {
-        std::cout <<
-            "Some error happened during parsing. Details below:\n" <<
-            e.what() << '\n';
-        std::cout << "Terminating parsing and query evaluation.\n";
-        exit(1);  // non-zero exit
-    }
+	// Convert the procedure
+	convertor.ProcedureReader(procedureLst);
 }
 
