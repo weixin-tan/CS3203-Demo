3e57cd247424bdc6894c2e09122e374d80b95a72
Juliet Teoh <teohqianying@gmail.com>
2022-03-14 00:05:44 +0800

    fix regression bug

--- Team35/Code35/src/spa/src/QueryRegex.cpp
+++ Team35/Code35/src/spa/src/QueryRegex.cpp
@@ -289,13 +289,13 @@ long findWithClause(const std::string& s){
 }
 
 long findPatternClauseInSubstring(const std::string& s, std::string original){
-  std::cout << s << "\n";
   long patternPosition = s.rfind("pattern");
   long bracketPosition = original.rfind("(");
   int patternLength = 7;
-  if (patternPosition != std::string::npos && patternPosition < bracketPosition){
+  if (patternPosition != std::string::npos){
     if (original.substr(patternPosition, original.length() - patternPosition).find('(') != std::string::npos &&
-        !stripString(original.substr(patternPosition+patternLength, bracketPosition-patternPosition-patternLength)).empty() ){
+        !stripString(original.substr(patternPosition+patternLength, bracketPosition-patternPosition-patternLength)).empty()
+        && patternPosition < bracketPosition){
       return patternPosition;
     }else{
       return findPatternClauseInSubstring(s.substr(0, patternPosition), original);
@@ -592,18 +592,15 @@ std::vector<std::string> splitPatternAndSuchThatClauses(const std::string& s){
     if (!stmt.empty()){
       if (!isWith(stmt)){
         returnList.push_back(stmt + ")");
-      }else if (isWith(stmt) && findPatternClauseInSubstring(stmt, stmt) != std::string::npos){
-        std::cout << "in here \n";
-        long x = findPatternClauseInSubstring(stmt, stmt);
+      }else if (isWith(stmt) && existSuchThat(stmt)){
+        long x = findSuchThatClause(stmt)[0];
         std::string first = stmt.substr(0, x);
         std::string second = stmt.substr(x, stmt.length()-x);
         temp = extractWithClauses(first);
         returnList.insert( returnList.end(), temp.begin(), temp.end());
         returnList.push_back(second + ")");
-      }else if (isWith(stmt) && existSuchThat(stmt)){
-        std::cout << "in here 1 \n";
-
-        long x = findSuchThatClause(stmt)[0];
+      }else if (isWith(stmt) && findPatternClauseInSubstring(stmt, stmt) != std::string::npos){
+        long x = findPatternClauseInSubstring(stmt, stmt);
         std::string first = stmt.substr(0, x);
         std::string second = stmt.substr(x, stmt.length()-x);
         temp = extractWithClauses(first);
