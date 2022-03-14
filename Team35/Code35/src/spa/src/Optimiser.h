25f0b7086e8ad2db8d081d043e32e3339f79948b
Vivegan <77456578+vivegank@users.noreply.github.com>
2022-03-14 23:28:18 +0800

    Create Optimiser and related Group class

---
+++ Team35/Code35/src/spa/src/Optimiser.h
@@ -0,0 +1,14 @@
+#ifndef SPA_OPTIMISER_H
+#define SPA_OPTIMISER_H
+
+#include "Result.h";
+#include "Group.h";
+
+class Optimiser {
+public:
+    Optimiser();
+    std::vector<Group> optimize(std::vector<Result> results);
+};
+
+
+#endif //SPA_OPTIMISER_H
