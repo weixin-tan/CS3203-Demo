99a92b1e1d7b636de1fabf6e1f6d1adcd04af797
prerthan99 <54735177+prerthan99@users.noreply.github.com>
2022-03-15 00:04:22 +0800

    Create table data structure to be used by result processor
    Table data structure

---
+++ Team35/Code35/src/spa/src/Table.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Prerthan Munireternam on 14/03/2022.
+//
+
+#include "Table.h"
+#include "Entity.h"
+#include "ProgramElement.h"
+
+Table::Table(){
+    Table::header = {};
+    Table::body = {};
+
+    //methods for header
+    std::vector<Entity> getHeader(){
+        return header;
+    }
+    void inputElement(Entity input,std::vector<Entity> header){
+        header.push_back(input);
+    }
+    //methods for body
+    std::vector<std::vector<ProgramElement>> getBody(){
+        return body;
+    }
+    void inputProgramElements(std::vector<ProgramElement> ProgramElements,std::vector<std::vector<ProgramElement>> body){
+        body.push_back(ProgramElements);
+    }
+}
---
+++ Team35/Code35/src/spa/src/Table.h
@@ -0,0 +1,28 @@
+//
+// Created by Prerthan Munireternam on 14/03/2022.
+//
+
+#ifndef SPA_TABLE_H
+#define SPA_TABLE_H
+
+
+#include <vector>
+#include "Entity.h"
+#include "ProgramElement.h"
+
+
+class Table {
+public:
+    //constructor
+    Table();
+    //attributes
+    std::vector<Entity> header;
+    std::vector<std::vector<ProgramElement>> body;
+    std::vector<Entity> getHeader();
+    void inputElement(Entity input,std::vector<Entity> header);
+    std::vector<std::vector<ProgramElement>> getBody();
+    void inputProgramElements(std::vector<ProgramElement> ProgramElements,std::vector<std::vector<ProgramElement>> body);
+};
+
+
+#endif //SPA_TABLE_H
