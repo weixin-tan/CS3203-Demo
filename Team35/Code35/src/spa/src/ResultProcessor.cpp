3a97820bc975cb07d1deeca25e9abd2ae2fbed69
prerthan99 <54735177+prerthan99@users.noreply.github.com>
2022-03-15 01:42:19 +0800

    Update ResultProcessor (1syn)
    1 syn wihout cross product for suchThat

--- Team35/Code35/src/spa/src/ResultProcessor.cpp
+++ Team35/Code35/src/spa/src/ResultProcessor.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "ResultProcessor.h"
 #include "Table.h"
+#include <set>
 
 ResultProcessor::ResultProcessor() = default;
 std::set<ProgramElement> processResults(std::vector<Result> results){
@@ -19,6 +20,11 @@ std::set<ProgramElement> processResults(std::vector<Result> results){
     std::vector<std::vector<ProgramElement>> body = table.body;
     //2 until last
     for (Result result : results) {  //for each result in the results list
+        //convert the programElement set into a programElement vector
+        //get the set
+        std::set<ProgramElement> programElementsSet = result.getOneSynSet();
+        //convert set to vector
+        std::vector<ProgramElement> programElementVector(programElementsSet.begin(), programElementsSet.end());
         //handle such that clause
         if(result.getResultType()==ResultType::SuchThatClause){
             //case where invalid
@@ -33,11 +39,19 @@ std::set<ProgramElement> processResults(std::vector<Result> results){
                 //check if entity is in header of table
                 if(std::find(header.begin(), header.end(), result.getOneSynEntity())
                     != header.end()){
-                    //header contains entity
+                    //header contains entity requiring cross product
                 } else{
                     //header does not contain entity
                     Entity entity = result.getOneSynEntity();
-                    table.inputElement(header,entity)
+                    table.inputElement(entity,table.getHeader());
+                }
+                //do similar thing for body of table
+                if(std::find(body.begin(), body.end(), programElementVector)
+                   != body.end()){
+                    //body contains program entities requiring cross product
+                } else{
+                    //body does not contain programElement
+                    table.inputProgramElements(programElementVector,table.getBody());
                 }
             //case where we handle twoSyn
             }else if(!result.getTwoSynSet().empty()){
