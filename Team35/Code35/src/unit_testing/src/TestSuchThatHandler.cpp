b2686e6b74794bf9f182b16d2ac42075a964e984
Vivegan <77456578+vivegank@users.noreply.github.com>
2022-03-17 05:40:51 +0800

    Complete writing tests for SuchThatHandler

--- Team35/Code35/src/unit_testing/src/TestSuchThatHandler.cpp
+++ Team35/Code35/src/unit_testing/src/TestSuchThatHandler.cpp
@@ -6,12 +6,6 @@
 #include "PKB.h"
 
 
-
-
-
-
-
-
 // Source for testing
 //procedure f {
 //  x = 5;  // 1
@@ -66,15 +60,7 @@ Entity fixedIntIf = Entity(EntityType::FixedInteger, "6");
 
 Entity wc = Entity(EntityType::Wildcard, "_");
 
-RelationshipRef callsRef_procSyn_procSyn = RelationshipRef(RelationshipType::Calls, procSyn1, procSyn2); // Calls(p1, p2)
-RelationshipRef callsRef_procSyn_fixedStr = RelationshipRef(RelationshipType::Calls, procSyn1, fixedStrProcG); // Calls(f, "g")
-RelationshipRef callsRef_procSyn_wc = RelationshipRef(RelationshipType::Calls, procSyn1, wc); // Calls(p1, _)
-RelationshipRef callsRef_fixedStr_procSyn = RelationshipRef(RelationshipType::Calls, fixedStrProcF, procSyn1); // Calls("f", p1)
-RelationshipRef callsRef_fixedStr_fixedStr = RelationshipRef(RelationshipType::Calls, fixedStrProcF, fixedStrProcG); // Calls("f", "g")
-RelationshipRef callsRef_fixedStr_wc = RelationshipRef(RelationshipType::Calls, fixedStrProcF, wc); // Calls("f", _)
-RelationshipRef callsRef_wc_procSyn = RelationshipRef(RelationshipType::Calls, wc, procSyn1); // Calls(_, p1)
-RelationshipRef callsRef_wc_fixedStr = RelationshipRef(RelationshipType::Calls, wc, fixedStrProcG); // Calls(_, "g")
-RelationshipRef callsRef_wc_wc = RelationshipRef(RelationshipType::Calls, wc, wc); // Calls (_, _)
+
 
 RelationshipRef followsRef_stmtSyn_stmtSyn = RelationshipRef(RelationshipType::Follows, printSyn, callSyn); // Follows(p, call)
 RelationshipRef followsTRef_stmtSyn_fixedInt = RelationshipRef(RelationshipType::FollowsT, assignSyn, fixedIntCall); // Follows*(a, "12")
@@ -119,6 +105,9 @@ TEST_CASE("Testing Modifies/Uses Relationships"){
     Result result11 = st->handleSuchThat(modifiesRef_stmtSyn_wc);// Modifies(s, _)
     Result result12 = st->handleSuchThat(usesRef_fixedInt_wc);// Uses("6", _)
 
+    Result expectedResultTrue;
+    expectedResultTrue.setValid(true);
+
     Result expectedResult1;
     expectedResult1.setValid(true);
     expectedResult1.setTwoSynEntities(std::pair<Entity, Entity>(procSyn1, varSyn));
@@ -158,18 +147,12 @@ TEST_CASE("Testing Modifies/Uses Relationships"){
     result5Elements.insert(ProgramElement::createProcedure("f"));
     expectedResult5.setOneSynSet(result5Elements);
 
-    Result expectedResult6;
-    expectedResult6.setValid(true);
-
     Result expectedResult7;
     expectedResult7.setValid(true);
     expectedResult7.setOneSynEntity(ifSyn);
     std::set<ProgramElement> result7Elements;
     result7Elements.insert(ProgramElement::createStatement(ElementType::kIf, 6));
     expectedResult7.setOneSynSet(result7Elements);
-
-    Result expectedResult8;
-    expectedResult8.setValid(true);
 
     Result expectedResult9;
     expectedResult9.setValid(true);
@@ -178,12 +161,10 @@ TEST_CASE("Testing Modifies/Uses Relationships"){
     result9Elements.insert(ProgramElement::createProcedure("f"));
     expectedResult9.setOneSynSet(result9Elements);
 
-    Result expectedResult10;
-    expectedResult10.setValid(true);
-
     Result expectedResult11;
     expectedResult11.setValid(true);
     expectedResult11.setOneSynEntity(stmtSyn);
+
     std::set<ProgramElement> result11Elements;
     result11Elements.insert(ProgramElement::createStatement(ElementType::kStatement, 1));
     result11Elements.insert(ProgramElement::createStatement(ElementType::kStatement, 2));
@@ -194,27 +175,151 @@ TEST_CASE("Testing Modifies/Uses Relationships"){
     result11Elements.insert(ProgramElement::createStatement(ElementType::kStatement, 9));
     result11Elements.insert(ProgramElement::createStatement(ElementType::kStatement, 10));
 
-    Result expectedResult12;
-    expectedResult12.setValid(true);
-
     REQUIRE(result1 == expectedResult1);
     REQUIRE(result2 == expectedResult2);
     REQUIRE(result3 == expectedResult3);
     REQUIRE(result4 == expectedResult4);
     REQUIRE(result5 == expectedResult5);
-    REQUIRE(result6 == expectedResult6);
+    REQUIRE(result6 == expectedResultTrue);
     REQUIRE(result7 == expectedResult7);
-    REQUIRE(result8 == expectedResult8);
+    REQUIRE(result8 == expectedResultTrue);
     REQUIRE(result9 == expectedResult9);
-    REQUIRE(result10 == expectedResult10);
+    REQUIRE(result10 == expectedResultTrue);
     REQUIRE(result11 == expectedResult11);
-    REQUIRE(result12 == expectedResult12);
+    REQUIRE(result12 == expectedResultTrue);
 }
 
 TEST_CASE("Testing Calls(*) Relationships"){
-    //Result result1 = st->handleSuchThat(modifiesRef_fixedInt_fixedStr);
+    SuchThatHandler* st = new SuchThatHandler(pkb.getPkbGetterStub());
+
+    RelationshipRef callsRef_procSyn_procSyn = RelationshipRef(RelationshipType::Calls, procSyn1, procSyn2); // Calls(p1, p2)
+    RelationshipRef callsRef_procSyn_fixedStr = RelationshipRef(RelationshipType::Calls, procSyn1, fixedStrProcG); // Calls(p1, "g")
+    RelationshipRef callsRef_procSyn_wc = RelationshipRef(RelationshipType::Calls, procSyn1, wc); // Calls(p1, _)
+    RelationshipRef callsRef_fixedStr_procSyn = RelationshipRef(RelationshipType::Calls, fixedStrProcF, procSyn1); // Calls("f", p1)
+    RelationshipRef callsRef_fixedStr_fixedStr = RelationshipRef(RelationshipType::Calls, fixedStrProcF, fixedStrProcG); // Calls("f", "g")
+    RelationshipRef callsRef_fixedStr_wc = RelationshipRef(RelationshipType::Calls, fixedStrProcF, wc); // Calls("f", _)
+    RelationshipRef callsRef_wc_procSyn = RelationshipRef(RelationshipType::Calls, wc, procSyn1); // Calls(_, p1)
+    RelationshipRef callsRef_wc_fixedStr = RelationshipRef(RelationshipType::Calls, wc, fixedStrProcG); // Calls(_, "g")
+    RelationshipRef callsRef_wc_wc = RelationshipRef(RelationshipType::Calls, wc, wc); // Calls (_, _)
+
+    Result result1 = st->handleSuchThat(callsRef_procSyn_procSyn);
+    Result result2 = st->handleSuchThat(callsRef_procSyn_fixedStr);
+    Result result3 = st->handleSuchThat(callsRef_procSyn_wc);
+    Result result4 = st->handleSuchThat(callsRef_fixedStr_procSyn);
+    Result result5 = st->handleSuchThat(callsRef_fixedStr_fixedStr);
+    Result result6 = st->handleSuchThat(callsRef_fixedStr_wc);
+    Result result7 = st->handleSuchThat(callsRef_wc_procSyn);
+    Result result8 = st->handleSuchThat(callsRef_wc_fixedStr);
+    Result result9 = st->handleSuchThat(callsRef_wc_wc);
+
+    Result expectedResultTrue;
+    expectedResultTrue.setValid(true);
+
+    Result expectedResultG;
+    expectedResultG.setValid(true);
+    expectedResultG.setOneSynEntity(procSyn1);
+    std::set<ProgramElement> g;
+    g.insert(ProgramElement::createProcedure("g"));
+    expectedResultG.setOneSynSet(g);
+
+    Result expectedResultF;
+    expectedResultF.setValid(true);
+    expectedResultF.setOneSynEntity(procSyn1);
+    std::set<ProgramElement> f;
+    f.insert(ProgramElement::createProcedure("f"));
+    expectedResultF.setOneSynSet(f);
+
+    Result expectedResultFG;
+    expectedResultFG.setValid(true);
+    expectedResultFG.setTwoSynEntities(std::pair<Entity, Entity>(procSyn1, procSyn2));
+    std::set<std::pair<ProgramElement, ProgramElement>> fg;
+    fg.insert(std::pair<ProgramElement, ProgramElement>(ProgramElement::createProcedure("f"), ProgramElement::createProcedure("g")));
+    expectedResultFG.setTwoSynSet(fg);
+
+    REQUIRE(result1 == expectedResultFG);
+    REQUIRE(result2 == expectedResultF);
+    REQUIRE(result3 == expectedResultF);
+    REQUIRE(result4 == expectedResultG);
+    REQUIRE(result5 == expectedResultTrue);
+    REQUIRE(result6 == expectedResultTrue);
+    REQUIRE(result7 == expectedResultG);
+    REQUIRE(result8 == expectedResultTrue);
+    REQUIRE(result9 == expectedResultTrue);
 }
 
 TEST_CASE("Testing Follows(*)/Parent(*)/Next(*)/Affects(*) Relationships"){
-    //Result result1 = st->handleSuchThat(modifiesRef_fixedInt_fixedStr);
+    SuchThatHandler* st = new SuchThatHandler(pkb.getPkbGetterStub());
+
+    RelationshipRef followsRef_stmtSyn_stmtSyn = RelationshipRef(RelationshipType::Follows, printSyn, callSyn); // Follows(p, call)
+    RelationshipRef followsTRef_stmtSyn_fixedInt = RelationshipRef(RelationshipType::FollowsT, assignSyn, fixedIntCall); // Follows*(a, "12")
+    RelationshipRef parentRef_stmtSyn_wc = RelationshipRef(RelationshipType::Parent, whileSyn, wc); //Parent(w, _)
+    RelationshipRef parentTRef_fixedInt_stmtSyn = RelationshipRef(RelationshipType::ParentT, fixedIntWhile,  assignSyn);//Parent*("4", a)
+    RelationshipRef nextRef_fixedInt_fixedInt = RelationshipRef(RelationshipType::Next, fixedIntPrint, fixedIntCall); //Next("11", "12")
+    RelationshipRef nextTRef_fixedInt_wc = RelationshipRef(RelationshipType::NextT, fixedIntStmt, wc); //Next*("1", _);
+    RelationshipRef affectsRef_wc_stmtSyn = RelationshipRef(RelationshipType::Affects, wc, assignSyn); // Affects(_, a)
+    RelationshipRef affectsTRef_wc_fixedInt = RelationshipRef(RelationshipType::AffectsT, wc, fixedIntAssign); // Affects*(_, "2")
+    RelationshipRef followsRef_wc_wc = RelationshipRef(RelationshipType::Follows, wc, wc); // Follows(_, _)
+
+    Result result1 = st->handleSuchThat(followsRef_stmtSyn_stmtSyn);
+    Result result2 = st->handleSuchThat(followsTRef_stmtSyn_fixedInt);
+    Result result3 = st->handleSuchThat(parentRef_stmtSyn_wc);
+    Result result4 = st->handleSuchThat(parentTRef_fixedInt_stmtSyn);
+    Result result5 = st->handleSuchThat(nextRef_fixedInt_fixedInt);
+    Result result6 = st->handleSuchThat(nextTRef_fixedInt_wc);
+    Result result7 = st->handleSuchThat(affectsRef_wc_stmtSyn);
+    Result result8 = st->handleSuchThat(affectsTRef_wc_fixedInt);
+    Result result9 = st->handleSuchThat(followsRef_wc_wc);
+
+    Result expectedResultTrue;
+    expectedResultTrue.setValid(true);
+
+    Result expectedResult1;
+    expectedResult1.setValid(true);
+    expectedResult1.setTwoSynEntities(std::pair<Entity, Entity>(printSyn, callSyn));
+    std::set<std::pair<ProgramElement, ProgramElement>> result1Elements;
+    result1Elements.insert(std::pair<ProgramElement, ProgramElement>(ProgramElement::createStatement(ElementType::kPrint, 11), ProgramElement::createStatement(ElementType::kCall, 12)));
+    expectedResult1.setTwoSynSet(result1Elements);
+
+    Result expectedResult2;
+    expectedResult2.setValid(true);
+    expectedResult2.setOneSynEntity(assignSyn);
+    std::set<ProgramElement> result2Elements;
+    result2Elements.insert(ProgramElement::createStatement(ElementType::kAssignment, 1));
+    result2Elements.insert(ProgramElement::createStatement(ElementType::kAssignment, 2));
+    result2Elements.insert(ProgramElement::createStatement(ElementType::kAssignment, 3));
+    expectedResult2.setOneSynSet(result2Elements);
+
+    Result expectedResult3;
+    expectedResult3.setValid(true);
+    expectedResult3.setOneSynEntity(whileSyn);
+    std::set<ProgramElement> result3Elements;
+    result3Elements.insert(ProgramElement::createStatement(ElementType::kWhile, 4));
+    expectedResult3.setOneSynSet(result3Elements);
+
+    Result expectedResult4;
+    expectedResult4.setValid(true);
+    expectedResult4.setOneSynEntity(assignSyn);
+    std::set<ProgramElement> result4Elements;
+    result4Elements.insert(ProgramElement::createStatement(ElementType::kAssignment, 7));
+    result4Elements.insert(ProgramElement::createStatement(ElementType::kAssignment, 8));
+    result4Elements.insert(ProgramElement::createStatement(ElementType::kAssignment, 9));
+    result4Elements.insert(ProgramElement::createStatement(ElementType::kAssignment, 10));
+    expectedResult4.setOneSynSet(result4Elements);
+    
+    Result expectedResult7;
+    expectedResult7.setValid(true);
+    expectedResult7.setOneSynEntity(assignSyn);
+    std::set<ProgramElement> result7Elements;
+    result7Elements.insert(ProgramElement::createStatement(ElementType::kAssignment, 2));
+    expectedResult7.setOneSynSet(result7Elements);
+
+    REQUIRE(result1 == expectedResult1);
+    REQUIRE(result2 == expectedResult2);
+    REQUIRE(result3 == expectedResult3);
+    REQUIRE(result4 == expectedResult4);
+    REQUIRE(result5 == expectedResultTrue);
+    REQUIRE(result6 == expectedResultTrue);
+    REQUIRE(result7 == expectedResult7);
+    REQUIRE(result8 == expectedResultTrue);
+    REQUIRE(result9 == expectedResultTrue);
 }
