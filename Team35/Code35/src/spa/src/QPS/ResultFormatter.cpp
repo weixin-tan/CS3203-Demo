3bdeea347f0760d43f890631c3837449bd403415
Vivegan <77456578+vivegank@users.noreply.github.com>
2022-03-28 03:17:08 +0800

    Update ResultFormatter to take in all return types

--- Team35/Code35/src/spa/src/QPS/ResultFormatter.cpp
+++ Team35/Code35/src/spa/src/QPS/ResultFormatter.cpp
@@ -2,9 +2,56 @@
 
 ResultFormatter::ResultFormatter() = default;
 
-std::list<std::string> ResultFormatter::formatResult(const std::vector<ProgramElement>& finalResult) {
+std::list<std::string> ResultFormatter::formatResult(FormattedResult finalResult) {
     std::list<std::string> result;
-    for (const ProgramElement& e : finalResult)
-        result.push_back(e.toString());  // toString can take in EntityAttributeType to specify
+    if (!finalResult.getValid()) {
+        if (finalResult.getResultType() == FormattedResultType::BOOLEAN) {
+            result.emplace_back("FALSE");
+        }
+    } else if (finalResult.getResultType() == FormattedResultType::BOOLEAN) {
+        result.emplace_back("TRUE");
+    } else if (finalResult.getResultType() == FormattedResultType::SINGLE) {
+        result = handleSingle(finalResult.getEntityList()[0], finalResult.getProgramElementLists()[0]);
+        return result;
+    } else if (finalResult.getResultType() == FormattedResultType::TUPLE) {
+        result = handleTuple(finalResult.getEntityList(), finalResult.getProgramElementLists());
+
+    } else {
+
+    }
+    return result;
+}
+
+std::list<std::string> ResultFormatter::handleSingle(Entity entity, std::vector<ProgramElement> elements) {
+    std::list<std::string> result;
+    if (entity.aType == EntityAttributeType::NULL_ATTRIBUTE) {
+        for (const ProgramElement& e : elements) {
+            result.push_back(e.toString());
+        }
+    } else {
+        for (const ProgramElement& e : elements) {
+            result.push_back(e.toString(entity.aType));
+        }
+    }
+    return result;
+}
+
+std::list<std::string>
+ResultFormatter::handleTuple(std::vector<Entity> entities, std::vector<std::vector<ProgramElement>> lists) {
+    std::list<std::string> result;
+    for (int i = 0; i < lists[0].size(); i++) {
+        std::string str;
+        for (int j = 0; j < entities.size(); j++) {
+            if (entities[j].aType == EntityAttributeType::NULL_ATTRIBUTE) {
+                str += lists[j][i].toString();
+            } else {
+                str += lists[j][i].toString(entities[j].aType);
+            }
+            if (j != entities.size() - 1) {
+                str += " ";
+            }
+        }
+        result.push_back(str);
+    }
     return result;
 }
