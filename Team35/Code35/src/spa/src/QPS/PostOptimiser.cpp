b928f2dc85531814e1e2414dd14326da7db07c9f
Vivegan <77456578+vivegank@users.noreply.github.com>
2022-03-29 02:40:13 +0800

    Fix bug in BOOLEAN handling

--- Team35/Code35/src/spa/src/QPS/PostOptimiser.cpp
+++ Team35/Code35/src/spa/src/QPS/PostOptimiser.cpp
@@ -3,7 +3,12 @@
 PostOptimiser::PostOptimiser() = default;
 
 std::vector<ResultGroup> PostOptimiser::optimise(std::vector<ResultGroup> resultGroups) {
+    if (resultGroups.size() == 1) {
+        return resultGroups;
+    }
     std::vector<ResultGroup> groups;
+    groups.push_back(resultGroups[0]);
+    resultGroups.erase(resultGroups.begin());
     for (const auto& r : resultGroups) {
         std::vector<Result> results = r.getGroup();
         std::vector<Result> optimisedResults;
