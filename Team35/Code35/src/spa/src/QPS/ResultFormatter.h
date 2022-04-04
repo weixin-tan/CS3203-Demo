d92ff8fd1c2c55cbe0e9369b223ca525a0d00f70
prerthan99 <54735177+prerthan99@users.noreply.github.com>
2022-04-04 17:41:35 +0800

    added hashfunction
    sorry, forgot to push the commit on saturday

--- Team35/Code35/src/spa/src/QPS/ResultFormatter.h
+++ Team35/Code35/src/spa/src/QPS/ResultFormatter.h
@@ -14,7 +14,11 @@ public:
     std::list<std::string> formatResult(FormattedResult finalResult);
 
 private:
+<<<<<<< Updated upstream
     std::list<std::string> handleSingle(Entity entity, std::vector<ProgramElement> elements);
+=======
+    std:: list <std:: string> handleSingle(Entity entity, std:: vector <ProgramElement> elements);
+>>>>>>> Stashed changes
     std::list<std::string> handleTuple(std::vector<Entity> entities, std::vector<std::vector<ProgramElement>> lists);
 };
 
--- Team35/Code35/src/spa/src/QPS/ResultProcessor.h
+++ Team35/Code35/src/spa/src/QPS/ResultProcessor.h
@@ -17,12 +17,22 @@ public:
 private:
     Table buildIntermediateTable(std::vector<Result> results);
     Table buildFinalTable(std::vector<Table> tables);
+<<<<<<< Updated upstream
     std::set<Entity> extractEntitySet(std::vector<Result> resultList);
     FormattedResult handleInvalidResult(Result r);
     FormattedResult handleZeroClause(std::vector<Result> resultList);
     FormattedResult extractTableInformation(std::vector<Entity> entities, Table table);
     std::vector<Entity> extractTableEntities(Table table);
     std::vector<Entity> extractOrderedEntities(std::vector<Result> results);
+=======
+    std:: set <Entity> extractEntitySet(std::vector <Result> resultList);
+    FormattedResult handleInvalidResult(Result r);
+    FormattedResult handleZeroClause(std::vector <Result> resultList);
+    FormattedResult extractTableInformation(std::vector<Entity> entities, Table table);
+    std::vector <Entity> extractTableEntities(Table table);
+
+    std::vector <Entity> extractOrderedEntities(std::vector <Result> results);
+>>>>>>> Stashed changes
 };
 
 #endif //SPA_RESULTPROCESSOR_H
