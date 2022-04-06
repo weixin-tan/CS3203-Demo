a3c57b966414b8ffae1783b0bad15b23dc6df5e3
prerthan99 <54735177+prerthan99@users.noreply.github.com>
2022-04-06 14:35:09 +0800

    adding hash_feature
    updated hashing to include hash function for program element and cantorPairingFunctio for hashing int

--- Team35/Code35/src/spa/src/QPS/Hashing.cpp
+++ Team35/Code35/src/spa/src/QPS/Hashing.cpp
@@ -4,11 +4,50 @@
 
 #include <random>
 #include <vector>
+#include <algorithm>
 
 #include "Hashing.h"
+#include "Entity.h"
 
-int Hashing::hashFunction(Entity entity) {
+int Hashing::hashFunctionForEntities(Entity entity){
+    // declare n to be estimate of how many entities are present
     int n = getTotalEntitiesEstimate();
+    //get the upper bound for range of ints
+    int size = ceil( (n)*(log10(n*1.00)) );
+    //use hardware to generate random seed
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    //create uniform distribution of int from 1 to upper bound
+    std::uniform_int_distribution<> distr(1, size);
+    //vector to store the numbers
+    std::vector<int> storage;
+
+    //generate 5 random numbers from 1 to nlgn
+    //store these numbers in the storage vector
+    for(int n = 0; n<5; n++){
+        int randomInt = distr(gen);
+        storage.push_back(randomInt);
+    }
+
+    //shuffle storage
+    auto rng = std::default_random_engine {};
+    std::shuffle(std::begin(storage), std::end(storage), rng);
+
+    //get  a and b
+    int a = storage[2];
+    int b = storage[4];
+
+    //cantorPairingFunction to hash 2 int together
+    int ans = cantorPairingFunction(a, b);
+
+    //return the ans
+    return ans;
+
+}
+
+
+int Hashing::hashFunctionForProgramElements(ProgramElement programElement) {
+    int n = getTotalProgramElementsEstimate();
     int size = ceil( (n)*(log10(n*1.00)) );
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -20,16 +59,22 @@ int Hashing::hashFunction(Entity entity) {
         storage.push_back(randomInt);
     }
 
-    int ans = 0;
+    //shuffle storage
+    auto rng = std::default_random_engine {};
+    std::shuffle(std::begin(storage), std::end(storage), rng);
 
-    for(int i = 0; i < storage.size(); i++){
-        int num = storage[i];
-        ans = ans + num;
-    }
+    //get  a and b
+    int a = storage[2];
+    int b = storage[4];
 
+    //cantorPairingFunction to hash 2 int together
+    int ans = cantorPairingFunction(a, b);
+
+    //return the ans
     return ans;
 
 }
+
 
 int Hashing::getTotalEntitiesEstimate() {
     return totalEntitiesEstimate;
@@ -38,4 +83,21 @@ int Hashing::getTotalEntitiesEstimate() {
 void Hashing::setTotalEntitiesEstimate(int input) {
     totalEntitiesEstimate = input;
 }
+
+int Hashing::getTotalProgramElementsEstimate() {
+    return totalProgramElementsEstimate;
+}
+
+void Hashing::setTotalProgramElementsEstimate(int input) {
+    totalProgramElementsEstimate = input;
+}
+
+int Hashing::cantorPairingFunction(int a, int b) {
+    int ans = 0;
+    ans = ceil(((a+b)*(a+b+1))/2);
+    return ans;
+}
+
+
+
 
--- Team35/Code35/src/spa/src/QPS/Hashing.h
+++ Team35/Code35/src/spa/src/QPS/Hashing.h
@@ -6,17 +6,23 @@
 #define SPA_HASHING_H
 
 #include "Entity.h"
+#include "../PKB/ProgramElement.h"
 
 class Hashing{
 public:
     //constructor
     Entity entity; //entity to be hashed
     int totalEntitiesEstimate; //estimate how many entites would be present
+    int totalProgramElementsEstimate; //estimate how many programelements would be present
 
     //methods
-    int hashFunction(Entity entity);
+    int hashFunctionForEntities(Entity entity);
+    int hashFunctionForProgramElements(ProgramElement programElement);
     int getTotalEntitiesEstimate();
+    int getTotalProgramElementsEstimate();
     void setTotalEntitiesEstimate(int input);
+    void setTotalProgramElementsEstimate(int input);
+    int cantorPairingFunction(int a, int b);
 };
 
 #endif //SPA_HASHING_H
