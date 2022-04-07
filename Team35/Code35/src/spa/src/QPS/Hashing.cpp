ac465a2e48adb653fcf76235769223fcc06e25ee
prerthan99 <54735177+prerthan99@users.noreply.github.com>
2022-04-07 21:44:36 +0800

    update to maintain coding standard
    removed comments from hashing

--- Team35/Code35/src/spa/src/QPS/Hashing.cpp
+++ Team35/Code35/src/spa/src/QPS/Hashing.cpp
@@ -10,37 +10,36 @@
 #include "Entity.h"
 
 int Hashing::hashFunctionForEntities(Entity entity){
-    // declare n to be estimate of how many entities are present
+
     int n = getTotalEntitiesEstimate();
-    //get the upper bound for range of ints
+
     int size = ceil( (n)*(log10(n*1.00)) );
-    //use hardware to generate random seed
+
     std::random_device rd;
     std::mt19937 gen(rd());
-    //create uniform distribution of int from 1 to upper bound
+
     std::uniform_int_distribution<> distr(1, size);
-    //vector to store the numbers
+
     std::vector<int> storage;
 
-    //generate 5 random numbers from 1 to nlgn
-    //store these numbers in the storage vector
+
     for(int n = 0; n<5; n++){
         int randomInt = distr(gen);
         storage.push_back(randomInt);
     }
 
-    //shuffle storage
+
     auto rng = std::default_random_engine {};
     std::shuffle(std::begin(storage), std::end(storage), rng);
 
-    //get  a and b
+
     int a = storage[2];
     int b = storage[4];
 
-    //cantorPairingFunction to hash 2 int together
+
     int ans = cantorPairingFunction(a, b);
 
-    //return the ans
+
     return ans;
 
 }
@@ -59,18 +58,18 @@ int Hashing::hashFunctionForProgramElements(ProgramElement programElement) {
         storage.push_back(randomInt);
     }
 
-    //shuffle storage
+
     auto rng = std::default_random_engine {};
     std::shuffle(std::begin(storage), std::end(storage), rng);
 
-    //get  a and b
+
     int a = storage[2];
     int b = storage[4];
 
-    //cantorPairingFunction to hash 2 int together
+
     int ans = cantorPairingFunction(a, b);
 
-    //return the ans
+
     return ans;
 
 }
